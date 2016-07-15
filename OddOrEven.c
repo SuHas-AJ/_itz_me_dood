5be31deaf983e4d22405c918b9ab04f6e169789f
SuHas-AJ <ajithkumarthalapolavaruma@gmail.com>
2016-07-15 20:33:50 +0530

    Add files via upload

---
+++ OddOrEven.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+#include<conio.h>
+
+int main(){
+    
+	int var;
+
+   	clrscr();
+
+    	scanf("%d",&var);
+
+    	if(var%2==0){
+
+        printf("%d is an even number",var);
+	}
+
+    	else{
+   
+     	printf("%d is an odd number",var);
+   
+ 	}
+
+    	getch();
+  
+  	return 0;
+
+}
+
+
